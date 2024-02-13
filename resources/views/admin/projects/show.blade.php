ab6d690ea34d1db2673dcb3ec51497d3161217fa
OgataiKhan <badellino.kevin@gmail.com>
2024-02-13 07:31:02 +0100

    feat: show view

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -5,6 +5,17 @@
         <h1>{{ $project->title }}</h1>
         <p>{{ $project->description }}</p>
         <p>Type: {{ $project->type->title ?? 'None' }}</p>
+        <div>
+            Technologies used: 
+            <ul>
+                @foreach ($project->technologies as $technology)
+                    <li>
+                        {{ $technology->title }}
+                    </li>
+                @endforeach
+            </ul>
+            
+        </div>
         <a href="{{ route('admin.projects.index') }}" role="button" class="btn btn-primary">Projects list</a>
     </div>
 @endsection
