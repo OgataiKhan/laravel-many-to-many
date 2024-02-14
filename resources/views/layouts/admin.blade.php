850132f6483c734e5b3ed7b19728849a9e3aa3da
OgataiKhan <badellino.kevin@gmail.com>
2024-02-14 06:30:51 +0100

    feat: technologies sidebar link

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -69,6 +69,12 @@
                                     <i class="fa-solid fa-tachometer-alt fa-lg fa-fw"></i> Projects
                                 </a>
                             </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-white {{ Route::currentRouteName() == 'admin.technologies.index' ? 'bg-secondary' : '' }}"
+                                    href="{{ route('admin.technologies.index') }}">
+                                    <i class="fa-solid fa-tachometer-alt fa-lg fa-fw"></i> Technologies
+                                </a>
+                            </li>
                         </ul>
 
 
